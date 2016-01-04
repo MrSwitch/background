ed356a7f30305d93eba5aa9aea209fa5dffb84bf
Andrew Dodson <sonofdod@gmail.com>
2016-01-04 13:46:40 +0000

    update comments

--- demo/init.js
+++ demo/init.js
@@ -5,6 +5,7 @@ var bg;
 // Push a function to call
 background.push(function(BG) {
 	// Passing in null for target inserts it into the body background
+	// Defaults to the only background stage added.
 	bg = BG.init(null);
 
 	// Set default state
